f0813229be2688ba2ff1dc3897d9367162503738
apexnt <apexnt@yandex.ru>
2022-07-11 12:21:08 +0500

    add CRUD

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -2,9 +2,12 @@ package ru.netology.nmedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.activity.viewModels
-import ru.netology.nmedia.data.imp.PostsAdapter
+import ru.netology.nmedia.adapter.PostsAdapter
 import ru.netology.nmedia.databinding.ActivityMainBinding
+import ru.netology.nmedia.util.hideKeyboard
+import ru.netology.nmedia.util.showKeyboard
 import ru.netology.nmedia.viewModel.PostViewModel
 
 class MainActivity : AppCompatActivity() {
@@ -16,10 +19,41 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val adapter = PostsAdapter(viewModel::onLikeClicked, viewModel::onShareClicked)
+        val adapter = PostsAdapter(viewModel)
         binding.postsRecyclerView.adapter = adapter
         viewModel.data.observe(this) { posts ->
             adapter.submitList(posts)
+        }
+
+        binding.saveButton.setOnClickListener {
+            with(binding.contentEditText) {
+                val content = binding.contentEditText.text.toString()
+                viewModel.onSaveButtonClicked(content)
+            }
+        }
+
+        binding.cancelButton.setOnClickListener {
+            with(binding.contentEditText) {
+                viewModel.onCancelEditButtonClicked()
+                val content = binding.contentEditText.text.toString()
+                viewModel.onSaveButtonClicked(content)
+            }
+        }
+
+        viewModel.currentPost.observe(this) { currentPost ->
+            with(binding.contentEditText) {
+                val content = currentPost?.content
+                setText(content)
+                if (content != null) {
+                    binding.contentGroup.visibility = View.VISIBLE
+                    requestFocus()
+                    showKeyboard()
+                } else {
+                    binding.contentGroup.visibility = View.GONE
+                    clearFocus()
+                    hideKeyboard()
+                }
+            }
         }
     }
 }
---
+++ app/src/main/java/ru/netology/nmedia/adapter/PostInteractionListener.kt
@@ -0,0 +1,11 @@
+package ru.netology.nmedia.adapter
+
+import ru.netology.nmedia.dto.Post
+
+interface PostInteractionListener {
+    fun onLikeClicked(post: Post)
+    fun onDeleteClicked(post: Post)
+    fun onShareClicked(post: Post)
+    fun onEditClicked(post: Post)
+
+}
